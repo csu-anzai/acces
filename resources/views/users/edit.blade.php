1a164fe97187c85fd3081120e1a9bba5cb1e1e9b
Bryl Kezter Lim <bryllim@gmail.com>
2019-08-14 01:42:21 +0800

    Fixed account management UI.

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -39,7 +39,7 @@
                 <div class="row">
                   <label class="col-sm-2 col-form-label">{{ __('First Name') }}</label>
                   <div class="col-sm-7">
-                    <div class="form-group{{ $errors->has('firstname') ? ' has-danger' : '' }}">
+                    <div class="form-group{{ $errors->has('firstname') ? ' has-danger' : ' has-success' }}">
                       <input class="form-control{{ $errors->has('firstname') ? ' is-invalid' : '' }}" name="firstname" id="input-firstname" type="text" placeholder="{{ __('First Name') }}" value="{{ old('firstname', $user->firstname) }}" required="true" aria-required="true"/>
                       @if ($errors->has('firstname'))
                         <span id="firstname-error" class="error text-danger" for="input-firstname">{{ $errors->first('firstname') }}</span>
@@ -50,7 +50,7 @@
                 <div class="row">
                   <label class="col-sm-2 col-form-label">{{ __('Last Name') }}</label>
                   <div class="col-sm-7">
-                    <div class="form-group{{ $errors->has('lastname') ? ' has-danger' : '' }}">
+                    <div class="form-group{{ $errors->has('lastname') ? ' has-danger' : ' has-success' }}">
                       <input class="form-control{{ $errors->has('lastname') ? ' is-invalid' : '' }}" name="lastname" id="input-lastname" type="text" placeholder="{{ __('Last Name') }}" value="{{ old('lastname', $user->lastname) }}" required="true" aria-required="true"/>
                       @if ($errors->has('lastname'))
                         <span id="lastname-error" class="error text-danger" for="input-lastname">{{ $errors->first('lastname') }}</span>
@@ -61,7 +61,7 @@
                 <div class="row">
                   <label class="col-sm-2 col-form-label">{{ __('Designation') }}</label>
                   <div class="col-sm-7">
-                    <div class="form-group{{ $errors->has('designation') ? ' has-danger' : '' }}">
+                    <div class="form-group{{ $errors->has('designation') ? ' has-danger' : ' has-success' }}">
                       <select class="form-control{{ $errors->has('designation') ? ' is-invalid' : '' }}" name="designation_id" id="input-designation" required>
                         <option disabled selected value="">Designation</option>
                         @foreach($designations as $designation)
@@ -78,7 +78,7 @@
                 <div class="row">
                   <label class="col-sm-2 col-form-label">{{ __('School') }}</label>
                   <div class="col-sm-7">
-                    <div class="form-group{{ $errors->has('school') ? ' has-danger' : '' }}">
+                    <div class="form-group{{ $errors->has('school') ? ' has-danger' : ' has-success' }}">
                       <select id="school_dropbox" class="form-control{{ $errors->has('school') ? ' is-invalid' : '' }}" name="school_id" id="input-school" required>
                         <option disabled selected value="">School</option>
                         @foreach($schools as $school)
@@ -95,7 +95,7 @@
                 <div class="row">
                   <label class="col-sm-2 col-form-label">{{ __('Department') }}</label>
                   <div class="col-sm-7">
-                    <div class="form-group{{ $errors->has('department') ? ' has-danger' : '' }}">
+                    <div class="form-group{{ $errors->has('department') ? ' has-danger' : ' has-success' }}">
                       <select id="department_dropbox" class="form-control{{ $errors->has('department') ? ' is-invalid' : '' }}" name="department_id" id="input-department" required>
                         <option disabled selected value="">Department</option>
                         @foreach($departments as $department)
@@ -112,7 +112,7 @@
                 <div class="row">
                   <label class="col-sm-2 col-form-label">{{ __('Organization') }}</label>
                   <div class="col-sm-7">
-                    <div class="form-group{{ $errors->has('organization') ? ' has-danger' : '' }}">
+                    <div class="form-group{{ $errors->has('organization') ? ' has-danger' : ' has-success' }}">
                       <select class="form-control{{ $errors->has('organization') ? ' is-invalid' : '' }}" name="organization_id" id="input-organization" required>
                         <option disabled selected value="">Organization</option>
                         @foreach($organizations as $organization)
@@ -129,7 +129,7 @@
                 <div class="row">
                   <label class="col-sm-2 col-form-label">{{ __('Username') }}</label>
                   <div class="col-sm-7">
-                    <div class="form-group{{ $errors->has('username') ? ' has-danger' : '' }}">
+                    <div class="form-group{{ $errors->has('username') ? ' has-danger' : ' has-success' }}">
                       <input class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" id="input-username" type="text" placeholder="{{ __('Username') }}" value="{{ old('username', $user->username) }}" required />
                       @if ($errors->has('username'))
                         <span id="username-error" class="error text-danger" for="input-username">{{ $errors->first('username') }}</span>
@@ -140,7 +140,7 @@
                 <div class="row">
                   <label class="col-sm-2 col-form-label">{{ __('Email') }}</label>
                   <div class="col-sm-7">
-                    <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
+                    <div class="form-group{{ $errors->has('email') ? ' has-danger' : ' has-success' }}">
                       <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" id="input-email" type="email" placeholder="{{ __('Email') }}" value="{{ old('email', $user->email) }}" required />
                       @if ($errors->has('email'))
                         <span id="email-error" class="error text-danger" for="input-email">{{ $errors->first('email') }}</span>
@@ -151,7 +151,7 @@
                 <div class="row">
                   <label class="col-sm-2 col-form-label">{{ __('Contact Number') }}</label>
                   <div class="col-sm-7">
-                    <div class="form-group{{ $errors->has('contact') ? ' has-danger' : '' }}">
+                    <div class="form-group{{ $errors->has('contact') ? ' has-danger' : ' has-success' }}">
                       <input class="form-control{{ $errors->has('contact') ? ' is-invalid' : '' }}" name="contact" id="input-contact" type="text" placeholder="{{ __('Contact Number') }}" value="{{ old('contact', $user->contact) }}" required="true" aria-required="true"/>
                       @if ($errors->has('contact'))
                         <span id="contact-error" class="error text-danger" for="input-contact">{{ $errors->first('contact') }}</span>
