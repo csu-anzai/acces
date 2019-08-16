c1bb26efe15f9c551233a33162579a2d47b872bf
MikoAninon <michael.aninon@gmail.com>
2019-08-16 22:42:17 +0800

    Temporarily changed table_list into proposal form

--- resources/views/pages/table_list.blade.php
+++ resources/views/pages/table_list.blade.php
@@ -6,273 +6,118 @@
     <div class="row">
       <div class="col-md-12">
         <div class="card">
-          <div class="card-header card-header-primary">
-            <h4 class="card-title ">Simple Table</h4>
-            <p class="card-category"> Here is a subtitle for this table</p>
+                <div class="card-header card-header-success">
+                  <h4 class="card-title " style="font-size:40px;">{{ __('Form A') }}</h4>
+                  <p class="card-category"> {{ __('Filling Out Form A: CES Program/Project/Activity Proposal') }}</p>
+                </div>
+        <div class="card-body">
+          @if (session('status'))
+            <div class="row">
+              <div class="col-sm-12">
+                <div class="alert alert-success">
+                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <i class="material-icons">close</i>
+                  </button>
+                    <span>{{ session('status') }}</span>
+                </div>
+              </div>
+            </div>
+          @endif
+          <div class="row">
+                  <label class="col-sm-2 col-form-label" style="color:black">{{ __('Title') }}</label>
+                  <div class="col-sm-7">
+                    <div class="form-group{{ $errors->has('title') ? ' has-danger' : ' has-success' }}">
+                      <input class="form-control" name="title" id="input-title" type="text" placeholder="{{ __('Name of the Program/Project/Activity') }}" value="" required="true" aria-required="true"/>
+                    </div>
+                  </div>
           </div>
-          <div class="card-body">
-            <div class="table-responsive">
-              <table class="table">
-                <thead class=" text-primary">
-                  <th>
-                    ID
-                  </th>
-                  <th>
-                    Name
-                  </th>
-                  <th>
-                    Country
-                  </th>
-                  <th>
-                    City
-                  </th>
-                  <th>
-                    Salary
-                  </th>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>
-                      1
-                    </td>
-                    <td>
-                      Dakota Rice
-                    </td>
-                    <td>
-                      Niger
-                    </td>
-                    <td>
-                      Oud-Turnhout
-                    </td>
-                    <td class="text-primary">
-                      $36,738
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      2
-                    </td>
-                    <td>
-                      Minerva Hooper
-                    </td>
-                    <td>
-                      Curaçao
-                    </td>
-                    <td>
-                      Sinaai-Waas
-                    </td>
-                    <td class="text-primary">
-                      $23,789
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      3
-                    </td>
-                    <td>
-                      Sage Rodriguez
-                    </td>
-                    <td>
-                      Netherlands
-                    </td>
-                    <td>
-                      Baileux
-                    </td>
-                    <td class="text-primary">
-                      $56,142
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      4
-                    </td>
-                    <td>
-                      Philip Chaney
-                    </td>
-                    <td>
-                      Korea, South
-                    </td>
-                    <td>
-                      Overland Park
-                    </td>
-                    <td class="text-primary">
-                      $38,735
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      5
-                    </td>
-                    <td>
-                      Doris Greene
-                    </td>
-                    <td>
-                      Malawi
-                    </td>
-                    <td>
-                      Feldkirchen in Kärnten
-                    </td>
-                    <td class="text-primary">
-                      $63,542
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      6
-                    </td>
-                    <td>
-                      Mason Porter
-                    </td>
-                    <td>
-                      Chile
-                    </td>
-                    <td>
-                      Gloucester
-                    </td>
-                    <td class="text-primary">
-                      $78,615
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+          <div class="row">
+                  <label class="col-sm-2 col-form-label" style="color:black">{{ __('CES Type') }}</label>
+                  <div class="col-sm-7">
+                    <div class="form-group" name="cestype" required>
+                      <select class="form-control" name="CES Type" required>
+                        <option disabled selected value="">CES Type</option>
+                        <option value="Program Based">Program Based</option>
+                        <option value="Program Based">Activity Based</option>                       
+                      </select>
+                    </div>
+                  </div>
+          </div>
+          <div class="row">
+                  <label class="col-sm-2 col-form-label" style="color:black">{{ __('Inclusive Date') }}</label>
+                  <div class="col-sm-7">
+                    <div class="form-group row">
+                      <input class="form-control col-sm-5" type="date" placeholder="from" required>
+                      <p style="width:20px"></p>
+                      <p style="font-size:20px">to</p>        
+                      <p style="width:20px"></p>
+                      <input class="form-control col-sm-5" type="date" required>
+                    </div>
+                  </div>
+          </div>
+          <div class="row">
+                  <label class="col-sm-2 col-form-label" style="color:black">{{ __('Venue') }}</label>
+                  <div class="col-sm-7">
+                    <div class="form-group{{ $errors->has('venue') ? ' has-danger' : ' has-success' }}">
+                      <input class="form-control" name="title" id="input-title" type="text" placeholder="{{ __('Where will the activity take place?') }}" value="" required="true" aria-required="true"/>
+                    </div>
+                  </div>
+          </div>
+          <!-- start of textareas and other things--> 
+          <div> 
+            <div>
+              <nav class="navbar navbar-light" style="background-color: #dfede3 !important;">
+                <span class="navbar-brand mb-0 h1">Rationale and Contextualization</span> 
+              </nav>
+            </div>
+            <div class="md-form">
+                <textarea id="form1" class="md-textarea form-control" rows="6"></textarea>
+                <label for="form1">Type here</label>
             </div>
           </div>
-        </div>
-      </div>
-      <div class="col-md-12">
-        <div class="card card-plain">
-          <div class="card-header card-header-primary">
-            <h4 class="card-title mt-0"> Table on Plain Background</h4>
-            <p class="card-category"> Here is a subtitle for this table</p>
-          </div>
-          <div class="card-body">
-            <div class="table-responsive">
-              <table class="table table-hover">
-                <thead class="">
-                  <th>
-                    ID
-                  </th>
-                  <th>
-                    Name
-                  </th>
-                  <th>
-                    Country
-                  </th>
-                  <th>
-                    City
-                  </th>
-                  <th>
-                    Salary
-                  </th>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>
-                      1
-                    </td>
-                    <td>
-                      Dakota Rice
-                    </td>
-                    <td>
-                      Niger
-                    </td>
-                    <td>
-                      Oud-Turnhout
-                    </td>
-                    <td>
-                      $36,738
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      2
-                    </td>
-                    <td>
-                      Minerva Hooper
-                    </td>
-                    <td>
-                      Curaçao
-                    </td>
-                    <td>
-                      Sinaai-Waas
-                    </td>
-                    <td>
-                      $23,789
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      3
-                    </td>
-                    <td>
-                      Sage Rodriguez
-                    </td>
-                    <td>
-                      Netherlands
-                    </td>
-                    <td>
-                      Baileux
-                    </td>
-                    <td>
-                      $56,142
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      4
-                    </td>
-                    <td>
-                      Philip Chaney
-                    </td>
-                    <td>
-                      Korea, South
-                    </td>
-                    <td>
-                      Overland Park
-                    </td>
-                    <td>
-                      $38,735
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      5
-                    </td>
-                    <td>
-                      Doris Greene
-                    </td>
-                    <td>
-                      Malawi
-                    </td>
-                    <td>
-                      Feldkirchen in Kärnten
-                    </td>
-                    <td>
-                      $63,542
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      6
-                    </td>
-                    <td>
-                      Mason Porter
-                    </td>
-                    <td>
-                      Chile
-                    </td>
-                    <td>
-                      Gloucester
-                    </td>
-                    <td>
-                      $78,615
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+          <div> 
+            <div>
+              <nav class="navbar navbar-light" style="background-color: #dfede3 !important;">
+                <span class="navbar-brand mb-0 h1">Goal, Objectives, and Outcomes</span>
+              </nav>
+            </div>
+            <div class="md-form">
+                <textarea id="form2" class="md-textarea form-control" rows="6"></textarea>
+                <label for="form2">Type here</label>
             </div>
           </div>
+          <div> 
+            <div>
+              <nav class="navbar navbar-light" style="background-color: #dfede3 !important;">
+                <span class="navbar-brand mb-0 h1">Participants, Partners and Beneficiaries</span>
+              </nav>
+            </div>
+            <div class="md-form">
+                <textarea id="form3" class="md-textarea form-control" rows="6"></textarea>
+                <label for="form3">Type here</label>
+            </div>
+          </div>
+          <div> 
+            <div>
+              <nav class="navbar navbar-light" style="background-color: #dfede3 !important;">
+                <span class="navbar-brand mb-0 h1">Outline of Activities</span>
+              </nav>
+            </div>
+            <!-- place code here -->
+          </div>
+          <div> 
+            <div>
+              <nav class="navbar navbar-light" style="background-color: #dfede3 !important;">
+                <span class="navbar-brand mb-0 h1">Budgetary Requirements</span>
+              </nav>
+            </div>
+            <!-- place code here -->
+          </div>
+          
         </div>
+        <div class="card-footer ml-auto mr-auto">
+                <button type="submit" class="btn btn-success">{{ __('Save') }}</button>
+        </div>        
       </div>
     </div>
   </div>
