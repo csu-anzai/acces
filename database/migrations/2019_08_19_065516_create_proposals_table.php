a51ac8b93f4501ce2409e0ccc502110e6ee8d982
Lee, Ye Hoo <chlogan6474@gmail.com>
2019-08-19 15:17:54 +0800

    add proposal table

---
+++ database/migrations/2019_08_19_065516_create_proposals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProposalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proposals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->enum('CES_type', array('Program Based', 'Activity Based'));
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('venue');
+            $table->json('proposal_json');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proposals');
+    }
+}
