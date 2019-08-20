bb9f8bfe1e7b24b98dd70deab29a719329f6def9
Bryl Kezter Lim <bryllim@gmail.com>
2019-08-20 12:24:55 +0800

    Routed name to edit profile.

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -19,7 +19,7 @@
   <div class="sidebar-wrapper">
     <ul class="nav">
       <div class="alert alert-success ml-3" role="alert">
-        <a href="#" class="alert-link">{{Auth::user()->firstname}} {{Auth::user()->lastname}}</a><br>
+        <a href="{{ route('profile.edit') }}" class="alert-link">{{Auth::user()->firstname}} {{Auth::user()->lastname}}</a><br>
         {{$result->name}}
       </div>
       <li class="nav-item{{ $activePage == 'dashboard' ? ' active' : '' }}">
