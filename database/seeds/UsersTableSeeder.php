74ca8b02ad3d55867dcf1e9e63303af9ca96e6a9
Bryl Kezter Lim <bryllim@gmail.com>
2019-08-27 17:44:31 +0800

    Added cocurricular seeder.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -28,5 +28,23 @@ class UsersTableSeeder extends Seeder
             'designation_id' => '10',
             'organization_id' => '1'
         ]);
+
+        DB::table('users')->insert([
+            'username' => 'cocurricular',
+            'firstname' => 'Mariana',
+            'lastname' => 'Matthews',
+            'contact' => '09123456789',
+            'email' => 'mariana@acces.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('secret'),
+            'created_at' => now(),
+            'updated_at' => now(),
+
+            //Foreign Key
+            'department_id' => '1',
+            'designation_id' => '1',
+            'organization_id' => '2'
+        ]);
+        
     }
 }
